c95c1ce57ab45bfc201dac463778b0e50ff78f6b
Aaron Mamparo <aaronmamparo@gmail.com>
2021-10-03 07:39:03 -0500

    cache the welcome message

--- src/data_service.py
+++ src/data_service.py
@@ -32,8 +32,11 @@ class DataService(AbstractDataService):
     def __init__(self, env: AbstractEnvironment = Environment()):
         self.__base_url = env.get_data_service_base_url()
         self.__room_id = env.get_jqbx_room_id()
+        self.__cached_welcome_message: Optional[str] = None
 
     def get_welcome_message(self) -> Optional[str]:
+        if self.__cached_welcome_message:
+            return self.__cached_welcome_message
         response = requests.get(self.__get_welcome_message_endpoint())
         if response.status_code == 404:
             return None
@@ -41,9 +44,11 @@ class DataService(AbstractDataService):
 
     def set_welcome_message(self, welcome_message: str) -> None:
         requests.post(self.__get_welcome_message_endpoint(), json={'welcome_message': welcome_message})
+        self.__cached_welcome_message = welcome_message
 
     def clear_welcome_message(self) -> None:
         requests.delete(self.__get_welcome_message_endpoint())
+        self.__cached_welcome_message = None
 
     def relink(self, track_id: str, markets: List[str]) -> Optional[Dict[str, str]]:
         response = requests.get(
